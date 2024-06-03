43bfb71ecaee9dd0be73fbbe6eac2b837c790bf8
Grzegorz Sieczkowski <biuro@greku.pl>
2024-06-03 19:20:14 +0200

    base app and data structure

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  providers: [AppService],
+  controllers: [AppController],
+})
+export class AppModule {}
