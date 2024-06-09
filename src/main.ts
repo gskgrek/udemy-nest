010321c31fcf2ad818c41159eb4344e66d772435
gskgrek <biuro@greku.pl>
2024-06-09 22:33:44 +0200

    CRUD + serialize interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // remove from body attrs that are not in related dto
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
